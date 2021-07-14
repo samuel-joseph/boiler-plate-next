e0415e23ef748805cbae5296d952f831dad9a0e3
arcanlte <pinangayjoel@gmail.com>
2021-07-13 20:34:12 -0400

    Data initiated

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -1,14 +1,14 @@
 import ArticleItem from "./ArticleItem";
-import articleStyles from "../styles/Article.module.css"
+import articleStyles from "../styles/Article.module.css";
 
-const ArticleList = ({articles}) => {
+const ArticleList = ({ articles }) => {
   return (
     <div className={articleStyles.grid}>
-      {articles.map(article =>
-        <ArticleItem article={article} />
-      )}
+      {articles.map((article) => (
+        <ArticleItem key={article.id} article={article} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ArticleList
+export default ArticleList;
---
+++ data.js
@@ -0,0 +1,32 @@
+export const articles = [
+  {
+    id: "1",
+    title: "This is title",
+    excerpt: "This is excerpt",
+    body: "This is body!",
+  },
+  {
+    id: "2",
+    title: "This is title",
+    excerpt: "This is excerpt",
+    body: "This is body!",
+  },
+  {
+    id: "3",
+    title: "This is title",
+    excerpt: "This is excerpt",
+    body: "This is body!",
+  },
+  {
+    id: "4",
+    title: "This is title",
+    excerpt: "This is excerpt",
+    body: "This is body!",
+  },
+  {
+    id: "5",
+    title: "This is title",
+    excerpt: "This is excerpt",
+    body: "This is body!",
+  },
+];
